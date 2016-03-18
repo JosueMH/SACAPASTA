de3ebdd8e0bc4d598e8e5e25ebe52bb05fad8ed8
JosueMH <josuquitaa@hotmail.com>
2016-03-18 12:21:06 +0100

    Lista de cajeros mediante arrayAdapter

--- app/src/main/java/es/marques/sacapasta/MainActivity.java
+++ app/src/main/java/es/marques/sacapasta/MainActivity.java
@@ -2,6 +2,8 @@ package es.marques.sacapasta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lvItems = (ListView) findViewById(R.id.listView_cajeros);
+        String [] cajeros = getResources().getStringArray(R.array.cajeros_nombre);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, cajeros);
+        lvItems.setAdapter(adapter);
     }
 }
