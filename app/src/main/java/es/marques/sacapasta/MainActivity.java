40a18ae95f508d81a1ed62ffdcde44651b8952fc
JosueMH <josuquitaa@hotmail.com>
2016-03-18 13:00:22 +0100

    ArrayAdapter con layout personalizado.

--- app/src/main/java/es/marques/sacapasta/MainActivity.java
+++ app/src/main/java/es/marques/sacapasta/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         ListView lvItems = (ListView) findViewById(R.id.listView_cajeros);
         String [] cajeros = getResources().getStringArray(R.array.cajeros_nombre);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, cajeros);
+                R.layout.layout_vista_simple_cajero,R.id.TextView_nombreCajero,cajeros);
         lvItems.setAdapter(adapter);
     }
 }
